9e698e43bc983b5efc5c3b0c3acebd188bc419fa
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-26 17:29:08 -0500

    Console Aplication Practice

---
+++ SQLServerUI/HelperConnection.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLibrary;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServerUI
+{
+    public static class HelperConnection
+    {
+        public static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName)!;
+
+            return output;
+        }
+
+        public static void ReadAllContacts(SqlCrud sql)
+        {
+            var rows = sql.GetAllUsers();
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{ row.Id }. { row.FirstName } ");
+            }
+
+        }
+    }
+}
---
+++ SQLServerUI/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using SQLServerUI;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
+
+
+SqlCrud sql = new SqlCrud(HelperConnection.GetConnectionString("Default"));
+
+HelperConnection.ReadAllContacts(sql); 
+
+
+
+Console.ReadLine();
+
