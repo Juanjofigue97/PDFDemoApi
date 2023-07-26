a2d5a3225e7cb3693b1250db89f55061cd56f550
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-26 17:28:06 -0500

    Api PDF

---
+++ ApiDemoPDF/Controllers/DocumentsController.cs
@@ -0,0 +1,61 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiDemoPDF.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        private static void ReadAllContacts(SqlCrud sql)
+        {
+            var rows = sql.GetAllUsers();
+        }
+
+        public DocumentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+        // GET: api/<DocumentsController>
+        [HttpGet]
+        public IEnumerable<BasicUserModel> Get()
+        {
+            var connection = _config.GetConnectionString("Default");
+            SqlCrud sql = new SqlCrud(connection);
+            
+            return sql.GetAllUsers(); 
+        }
+
+        // GET api/<DocumentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DocumentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DocumentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DocumentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ApiDemoPDF/Models/Document.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ApiDemoPDF.Models
+{
+    public class Document
+    {
+        public int? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public string? Area { get; set; }
+        public int? IdFile { get; set; }
+        public DateTime? Publication { get; set; }
+        public string? Version { get; set; }
+        public string? Tags { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public List<string>? KeyWords { get; set; }
+        public string? PathFile { get; set; }
+        public FileInfo? File { get; set; }
+    }
+
+
+public static class DocumentEndpoints
+{
+	public static void MapDocumentEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Document").WithTags(nameof(Document));
+
+        group.MapGet("/", () =>
+        {
+            return new [] { new Document() };
+        })
+        .WithName("GetAllDocuments")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            //return new Document { ID = id };
+        })
+        .WithName("GetDocumentById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", (int id, Document input) =>
+        {
+            return TypedResults.NoContent();
+        })
+        .WithName("UpdateDocument")
+        .WithOpenApi();
+
+        group.MapPost("/", (Document model) =>
+        {
+            //return TypedResults.Created($"/api/Documents/{model.ID}", model);
+        })
+        .WithName("CreateDocument")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            //return TypedResults.Ok(new Document { ID = id });
+        })
+        .WithName("DeleteDocument")
+        .WithOpenApi();
+    }
+}}
