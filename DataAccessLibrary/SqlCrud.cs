d72cfd56c24e34e8f0cfd8b44a35ba19dd413615
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-26 17:28:34 -0500

    Connection with Database
    Creation Models, DataAccess

---
+++ DataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlCrud
+    {
+        private readonly string _connectionString;
+        private SqlDataAccess db = new SqlDataAccess();
+        public SqlCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<BasicUserModel> GetAllUsers() 
+        {
+            string sql = "SELECT Id,FirstName,Password FROM dbo.Users";
+
+            return db.LoadData<BasicUserModel, dynamic>(sql, new { }, _connectionString);
+
+        }
+
+        //public BasicUserModel GetFullUserById(int id)
+        //{
+        //    throw new NotImplementedException;
+        //}
+
+    }
+}
---
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary;
+
+public class SqlDataAccess
+{
+    public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString)
+    {
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            // Query -> Ready
+            List<T> rows = connection.Query<T>(sqlStatement).ToList();
+            return rows;
+        }
+
+    }
+
+    public void SaveData<T>(string sqlStatement, T parameters, string connectionString)
+    {
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            // Execute -> Write
+            connection.Execute(sqlStatement, parameters);
+        }
+    }
+}
